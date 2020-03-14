2f16371442a1cdd0083576d9dd44e88fe7c1aeca
nathdipankar <dipankar.tifr@gmail.com>
2020-03-14 15:04:26 +0100

    Set up added back

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="univariate_linear_regression",
+      version="0.1.0",
+      description="Univariate linear regression of housing price against housing area",
+      author="Dipankar Nath",
+      packages=find_packages("src"),
+      package_dir={"": "src"},
+      author_email="dibyachakravorty@gmail.com",
+      install_requires=["scikit-learn==0.22.2",
+                        "pytest",
+                        "numpy"
+                        "scipy"
+                        ],
+      )
